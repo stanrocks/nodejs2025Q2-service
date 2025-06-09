88c5ec2b58d6209479d21e20f966f4f678e9af2b
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2025-06-10 01:45:58 +0500

    feat: implement add user and get all users

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'node:crypto';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { users } from '../db/db';
 
 @Injectable()
 export class UserService {
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const date = Date.now();
+
+    const user = {
+      id: randomUUID(),
+      version: 1,
+      createdAt: date,
+      updatedAt: date,
+      ...createUserDto,
+    };
+
+    users.set(user.id, user);
+    console.log('User created');
+
+    delete user.password;
+    return user;
   }
 
   findAll() {
-    return `This action returns all user`;
+    const usersPublicData = Array.from(users.values()).map(
+      (user) => delete user.password,
+    );
+    return usersPublicData;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  update(id: string, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} user`;
   }
 }
