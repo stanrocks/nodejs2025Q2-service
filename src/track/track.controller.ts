fc04e23ddb15b12ae2811f1a9456e026f513546d
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2025-06-10 02:58:38 +0500

    feat: implement code 204 on track delete

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -6,7 +6,10 @@ import {
   Patch,
   Param,
   Delete,
+  HttpCode,
 } from '@nestjs/common';
+import { StatusCodes } from 'http-status-codes';
+
 import { TrackService } from './track.service';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { UpdateTrackDto } from './dto/update-track.dto';
@@ -36,6 +39,7 @@ export class TrackController {
   }
 
   @Delete(':id')
+  @HttpCode(StatusCodes.NO_CONTENT)
   remove(@Param('id') id: string) {
     return this.trackService.remove(id);
   }
