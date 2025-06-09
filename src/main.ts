e1a87b1bb3475556f4469d8c3a4f8cef8ac782ef
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2025-06-09 22:24:37 +0500

    feat: add env handling and swagger to the server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const PORT = Number(process.env.PORT) || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Home Library API')
+    .setDescription('The Home Library API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, documentFactory);
+
+  await app.listen(PORT);
 }
+
 bootstrap();
